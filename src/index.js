d276efd66fc6cafe311ab4a900c12f86da4703d8
Heber <heberlazo@gmail.com>
2021-06-10 22:01:00 -0500

    Added fetch method

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import "./main.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import formView from "./module/form";
+import getData from "./module/getData";
+import UI from "./module/ui";
+
+const getForm = document.getElementById("form");
+getForm.appendChild(formView());
+
+const search = document.getElementById("searchCity");
+const btnSubmit = document.getElementById("submit");
+
+btnSubmit.addEventListener("click", (e) => {
+  const currentVal = search.value;
+  getData(currentVal);
+
+  e.preventDefault();
+});
---
+++ src/module/getData.js
@@ -0,0 +1,12 @@
+export default async function getInfo(city) {
+  const key = "8cceef8c56079ba82c43dbf3d0d9ec90";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("Fetch Error", error);
+  }
+}
