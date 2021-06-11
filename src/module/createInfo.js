1075cd73de9bc7a37bf339d1e8611f9bd3097cdc
Heber <heberlazo@gmail.com>
2021-06-11 12:35:52 -0500

    Added temp to view

--- src/module/createInfo.js
+++ src/module/createInfo.js
@@ -2,7 +2,7 @@ export default async (data) => {
   const cardTitle = document.getElementById("card-title");
   cardTitle.innerText = `${data.name}, ${data.sys.country}`;
   const cardText = document.getElementById("card-text");
-  cardText.innerText = data.weather[0].description;
+  cardText.innerText = `${data.weather[0].description},  F ${data.main.temp}`;
   const cardImg = document.getElementById("card-img-top");
   cardImg.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
 };
